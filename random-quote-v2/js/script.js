dab1affb1959bb380add1ff613bc1ba771cc6ec1
Scott Janda <scottjanda@gmail.com>
2018-07-04 23:01:40 -0400

    All functionality and formatting requirements met

--- random-quote-v2/js/script.js
+++ random-quote-v2/js/script.js
@@ -3,15 +3,39 @@ var quoteIndex
 var quotes = [
   {
     quote: "I have not failed. I've just found 10,000 ways that won't work.",
-    source: "Thomas Alva Edison"
+    source: "Thomas Alva Edison",
+    tags: ["Profund"]
     //https://www.cs.virginia.edu/~robins/quotes.html
   },
   {
     quote: "The optimist proclaims that we live in the best of all possible worlds, and the pessimist fears this is true.",
-    source: "James Branch Cabell"
+    source: "James Branch Cabell",
+    tags: ["Profound", "Philosophy"]
     //https://www.cs.virginia.edu/~robins/quotes.html
+  },
+  {
+    quote: "This is not the end. It is not even the beginning of the end. But it is, perhaps, the end of the beginning.",
+    source: "Winston Churchill",
+    year: "1992",
+    tags: ["Profund", "WWII"]
+    //http://www.historyinanhour.com/2011/11/22/famous-quotes-second-world-war/
+  },
+  {
+    quote: "You're dealing with a lot of silly people in the marketplace; it's like a great big casino and everyone else is boozing. If you can stick with Pepsi, you should be O.K.",
+    source: "Warren Buffett",
+    citation: "Interview in Forbes",
+    year: "1974",
+    tags: ["Funny","Investing"]
+    //https://www.thestreet.com/story/13207512/1/the-15-greatest-warren-buffett-quotes-of-all-time.html
+  },
+  {
+    quote: "Programs must be written for people to read, and only incidentally for machines to execute.",
+    source: "Harold Abelson",
+    citation: "Structure and Interpretation of Computer Programs",
+    year: "1984",
+    tags: ["Coding"]
+    //https://www.goodreads.com/quotes/tag/programming
   }
-  //Need to add 3 more quotes and 2 more parameters
 ];
 
 //This function returns a random quote from the quotes array
@@ -20,15 +44,33 @@ function getRandomQuote() {
   return quoteIndex;
 }
 
-//This funciton prints the random quote to the page with the included CSS styles
+//This funciton prints the random quote to the page with the specified CSS styles
 function printQuote() {
   getRandomQuote();
+
   var displayQuote = quotes[quoteIndex].quote;
   var displaySource = quotes[quoteIndex].source;
-  var message = '<p class="quote">'+[displayQuote]+'</p>';
-  message += '<p><class="source">'+[displaySource]+'</p>';
+  var displayYear = quotes[quoteIndex].year;
+  var displayCitation = quotes[quoteIndex].citation;
+  var message = '<p class="quote">'+displayQuote+'</p>';
+
+  message += '<p class="source">'+displaySource;
+  if (quotes[quoteIndex].citation !== undefined) {
+    message += "<span class = 'citation'>" + quotes[quoteIndex].citation + "</span>";
+  }
+  if (quotes[quoteIndex].year !== undefined) {
+    message += "<span class = 'year'>" + quotes[quoteIndex].year + "</span>";
+  }
+  //Code to display tags with each quote
+  if (quotes[quoteIndex].tags !== undefined) {
+    message += "<p>Tags: " + quotes[quoteIndex].tags + "</p>";
+  }
+
+  message += '</p>';
   document.getElementById('quote-box').innerHTML = message;
+  randomBackgroundColor ();
 }
+
 //This function produces a random background color
 function randomBackgroundColor () {
   var red = Math.floor(Math.random() * 256 );
@@ -38,11 +80,8 @@ function randomBackgroundColor () {
 }
 
 printQuote();
-randomBackgroundColor ();
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-//Exceeds expectations requirement: quotes change automatically after 30 seconds
-setInterval("window.location.reload()", 30000);
-
-//Original code and notes from the project start files follows:
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//This causes the quote to change automatically after 30 seconds
+setInterval("window.location.reload()", 30000);
